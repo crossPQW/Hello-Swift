08be7ff8cb7fd01693eba315dbe1c99edc0388f5
crossPQW <651024752@qq.com>
2015-07-06 18:19:38 +0800

    基本运算符10%

---
+++ 基本运算符/main.swift
@@ -0,0 +1,43 @@
+//
+//  main.swift
+//  基本运算符
+//
+//  Created by 黄少华 on 15/7/6.
+//  Copyright (c) 2015年 黄少华. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
+let i = 1+2
+
+//赋值运算
+let b = 100
+var a = 5
+a = b
+println(a)
+
+let (x, y) = (1, 3)
+
+//swift里面赋值运算不返回任何东西.
+//if x = y {
+//    
+//}
+
+let helloH = "你好" + "黄少华"
+
+println(helloH)
+
+let fuckSwift = "fuck" + "swift"
+
+println(fuckSwift)
+
+let dog: Character = "d"
+let cow: Character = "c"
+let person: String = "person"
+let haha : String = "haha"
+
+let sub = person + haha
+let dogCow = dog + cow
+let personcat = person + cat
