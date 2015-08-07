417b85bed7ba3615decc923e51d76b1b4c3a2209
huangshaohua <huangshaohua@xywy.com>
2015-08-07 18:02:36 +0800

    swift 2.0
    func

--- 函数functions/main.swift
+++ 函数functions/main.swift
@@ -19,3 +19,68 @@ func sayHello(personName:String)->String{
 
 sayHello("BKB")
 println(sayHello("caonimama"))
+
+func sayWorld(personName:String) ->String{
+    let result = "你好" + personName
+    
+//    println(result)
+    return result;
+}
+sayWorld("咩哈哈")
+
+print(sayWorld("哦啦啦\n"))
+
+
+func sumNumber(number1:Int,number2:Int) -> Int{
+    return number1 + number2
+}
+
+print(sumNumber(1, 2))
+
+//无参数的函数
+func sayHelloWorld() ->NSString
+{
+    return "hello World"
+}
+print(sayHelloWorld())
+
+
+
+//被调用的时候,一个函数的返回值可以被忽略
+//func printAndCount(stringToPrint:String) -> Int
+//{
+//    print(stringToPrint)
+////    return stringToPrint.characters.count
+//}
+
+
+//多个返回值函数
+func minMax(array:[Int])->(min:Int,max:Int){
+    var currentMin = array[0]
+    var currentMax = array[0]
+    
+    for value in array[1..<array.count]{
+        if value < currentMin{
+            currentMin = value
+        }else if value > currentMax{
+            currentMax = value
+        }
+    }
+    
+    return(currentMin, currentMax)
+}
+
+
+print(minMax([1,2,3,4,5,6,7]))
+
+
+
+
+
+
+
+
+
+
+
+
